f217f7eeb48bbb1dfe6ce9eb689527e9e7e9cd1c
Jacob Schwartz <jacob@jaschwartz.net>
2021-01-10 19:54:04 -0500

    Add requirements and setup files for packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="livapordata",
+    version="0.9.0",
+    author="Jacob Schwartz",
+    author_email="jacobas@princeton.edu",
+    description="Literature data on lithium, especially its vapor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cfe316/livapordata",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy >= 1.15",
+        "scipy >= 1.2",
+    ],
+)
